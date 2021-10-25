bb604853a821b2131ca7b437bc8d885a22bfa306
Atharav Upare <atharavupare@gmail.com>
2021-10-25 22:20:07 +0530

    Count the noof prime no in range

---
+++ Iterations/CountofPrimeno.java
@@ -0,0 +1,37 @@
+package Iterations;
+//You can remove above line "It give give error on other machine"
+
+
+import java.util.Scanner;
+
+public class CountofPrimeno {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Starting Number:");
+        int num1=sc.nextInt();
+        System.out.println("Enter the last Number of the range:");
+        int num2=sc.nextInt();
+        int check,count=0;
+        for(int i=num1;i<=num2;i ++)
+        {
+            check=0;
+            for(int j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    check++;
+
+                }
+            }
+                if(check==2)
+                {
+                    count++;
+                }
+            
+
+        }
+        System.out.print(count);
+    }
+    
+}
